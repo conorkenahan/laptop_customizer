482eb0ab690124a9b26a51f9601156d28b7f97c4
Conor <67439130+conorkenahan@users.noreply.github.com>
2020-08-18 19:08:13 -0400

    refactoring revisions

---
+++ src/SummaryOption.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+class SummaryOption extends React.Component {
+  state = {
+    selected: this.props.selected,
+  };
+  render() {
+    const summary = Object.keys(this.state.selected).map((feature, idx) => {
+      const featureHash = feature + "-" + idx;
+      const selectedOption = this.state.selected[feature];
+
+      return (
+        <div className="summary__option" key={featureHash}>
+          <div className="summary__option__label">{feature} </div>
+          <div className="summary__option__value">{selectedOption.name}</div>
+          <div className="summary__option__cost">
+            {USCurrencyFormat.format(selectedOption.cost)}
+          </div>
+        </div>
+      );
+    });
+    return summary;
+  }
+}
+
+export default SummaryOption;
